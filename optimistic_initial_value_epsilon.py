d49cad5d55b808946d7a4065456d9edffd937cea
Justin <jmtennenbaum@gmail.com>
2020-01-21 15:02:35 -0500

    adding simple RL algorithms

---
+++ optimistic_initial_value_epsilon.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Bandit:
+	def __init__(self,m,upper_limit):
+		self.m = m
+		self.mean = upper_limit
+		self.N = 0
+
+	def pull(self):
+		return np.random.randn() + self.m
+
+	def update(self,x):
+		self.N +=1
+		self.mean = (1-1.0)*self.mean + (1/self.N)*x
+
+def run_experiment(m1,m2,m3,eps,N):
+	bandits = [Bandit(m1,10), Bandit(m2,10), Bandit(m3,10)]
+	data = np.empty(N)
+
+	for i in range(N):
+		j = np.argmax([b.mean for b in bandits])
+		x = bandits[j].pull()
+		bandits[j].update(x)
+
+		#for the plot
+		data[i] = x
+	cumulative_average = np.cumsum(data)/ (np.arange(N) + 1)
+
+	#plot moving average CTR (click through rate)
+	plt.plot(cumulative_average)
+	plt.plot(np.ones(N)*m1)
+	plt.plot(np.ones(N)*m2)
+	plt.plot(np.ones(N)*m3)
+	plt.xscale('log')
+	plt.show()
+
+	for b in bandits:
+		print(b.mean)
+
+	return cumulative_average
+
+if __name__ == '__main__':
+	c_1 = run_experiment(1.0,2.0,3.0,0.1,100000)
+	c_05 = run_experiment(1.0,2.0,3.0,0.05,100000)
+	c_01 = run_experiment(1.0,2.0,3.0,0.01,100000)
+
+	#log scale plot
+	plt.plot(c_1,label= 'eps = 0.1')
+	plt.plot(c_05,label= 'eps = 0.05')
+	plt.plot(c_01,label= 'eps = 0.01')
+	plt.legend()
+	plt.show()
