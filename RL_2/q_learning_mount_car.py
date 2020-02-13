69964d9067e1ca5e995daf0480ae7e5cf404eb64
Justin <jmtennenbaum@gmail.com>
2020-02-12 20:06:18 -0500

    q learning mountcar finished

--- RL_2/q_learning_mount_car.py
+++ RL_2/q_learning_mount_car.py
@@ -5,51 +5,55 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from datatime import datetime
+from datetime import datetime
 from sklearn.pipeline import FeatureUnion
+from sklearn.linear_model import SGDRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.kernel_approximation import RBFSampler
 
 class FeatureTransformer:
-    def __init__(self, env):
-        observation_examples = np.random.random((20000, 4)) * 2 - 1
+    def __init__(self, env, n_components=500):
+        observation_examples = np.array([env.observation_space.sample() for x in range(10000)])
         scaler = StandardScaler()
         scaler.fit(observation_examples)
 
-        #used to convert a state to featurized reprsentation
-        #we use RBF kerns with different variance to cover different parts of the space
+        # Used to converte a state to a featurizes represenation.
+        # We use RBF kernels with different variances to cover different parts of the space
         featurizer = FeatureUnion([
-            ('rbf1', RBFSampler(gamma=5.0, n_components=500)),
-            ('rbf2', RBFSampler(gamma=2.0, n_components=500)),
-            ('rbf3', RBFSampler(gamma=1.0, n_components=500)),
-            ('rbf4', RBFSampler(gamma=0.5, n_components=500)),
-        ])
+                ("rbf1", RBFSampler(gamma=5.0, n_components=n_components)),
+                ("rbf2", RBFSampler(gamma=2.0, n_components=n_components)),
+                ("rbf3", RBFSampler(gamma=1.0, n_components=n_components)),
+                ("rbf4", RBFSampler(gamma=0.5, n_components=n_components))
+                ])
+        featurizer.fit_transform(scaler.transform(observation_examples))
 
-        featurizer.fit(scaler.transform(observation_examples))
         self.scaler = scaler
         self.featurizer = featurizer
 
+
     def transform(self, observations):
+        # print "observations:", observations
         scaled = self.scaler.transform(observations)
+        # assert(len(scaled.shape) == 2)
         return self.featurizer.transform(scaled)
 
 
 class Model:
-    def __init__(selfself, env, feature_transformer, learning_rate):
+    def __init__(self, env, feature_transformer, learning_rate):
         self.env = env
         self.models = []
         self.feature_transformer = feature_transformer
         for i in range(env.action_space.n):
             model = SGDRegressor(learning_rate=learning_rate)
-            model.partial_fit(feature_transformer.transform([env.reset]), [0])
+            model.partial_fit(feature_transformer.transform([env.reset()]), [0])
             self.models.append(model)
 
-    def predict(selfself,s):
+    def predict(self,s):
         X = self.feature_transformer.transform([s])
         assert (len(X.shape) == 2)
-        return np.array([m.predict(X)[0] for mod in self.models])
+        return np.array([mod.predict(X)[0] for mod in self.models])
 
-    def update(selfself, s):
+    def update(self, s, a, G):
         X = self.feature_transformer.transform([s])
         assert(len(X.shape) == 2)
         self.models[a].partial_fit(X, [G])
@@ -61,7 +65,7 @@ class Model:
         else:
             return np.argmax(self.predict(s))
 
-def play_one(model, eps, gamma):
+def play_one(model, env,eps, gamma):
     '''
     :return: list of states_rewards and total rewards
     '''
@@ -106,13 +110,14 @@ def plot_running_avg(totalrewards):
     N = len(totalrewards)
     running_avg = np.empty(N)
     for i in range(N):
-        running_avg[i] = totalrewards[max(0,t-100):(t+1)].mean()
+        running_avg[i] = totalrewards[max(0,i-100):(i+1)].mean()
     plt.plot(running_avg)
     plt.title('Running Average')
     plt.show()
 
 def main():
     env = gym.make('MountainCar-v0')
+    print('Env Created')
     ft = FeatureTransformer(env)
     model = Model(env, ft, 'constant')
     gamma = 0.99
@@ -121,7 +126,7 @@ def main():
     totalrewards = np.empty(N)
     for n in range(N):
         eps = 0.1*(0.97**n)
-        totalreward = play_one(model, eps, gamma)
+        totalreward = play_one(model, env, eps, gamma)
         totalrewards[n] = totalreward
         print('episode:', n, 'total reward: ', totalreward)
     print('avg reward for the last 100 episodes: ', totalrewards[-100:].mean())
